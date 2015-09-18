050b2b9457a5d4cf95d04afb1e232d84cefb5f92
Sebastian Aspegren <nethakaaru@gmail.com>
2015-09-18 14:56:06 +0200

    commented all methods

--- src/othello/Controller.java
+++ src/othello/Controller.java
@@ -11,6 +11,12 @@ public class Controller {
     private Board board;
     private AI cpu;
 
+    /**
+     * The constructor for the controller.
+     *
+     * @param board   an object representing the board.
+     * @param console an object representing the console.
+     */
     public Controller(Board board, Console console) {
         this.board = board;
         this.console = console;
@@ -18,26 +24,40 @@ public class Controller {
         play();
     }
 
+    /**
+     * A method that acts as the players turn. The player inputs a coordinate and the game tries to place a player piece there.
+     * If there is already another piece there the player is prompted to input a different coordinate.
+     * If the user press cancel the game will turn itself off.
+     */
     private void playerPlay() {
         String s = JOptionPane.showInputDialog("Where do you want to place your piece? (3,3 for example)");
-        if(s==null)
+        if (s == null)
             System.exit(0);
         String[] split = s.split(",");
 
-       if(!board.play(Integer.parseInt(split[0]), Integer.parseInt(split[1]), 1))
-           playerPlay();
+        if (!board.play(Integer.parseInt(split[0]), Integer.parseInt(split[1]), 1))
+            playerPlay();
     }
 
-    private void cpuPlay(){
+    /**
+     *A method representing the computers move.
+     */
+    private void cpuPlay() {
         String cpuPlay = cpu.play(board.getPlayField());
-        String [] split2 = cpuPlay.split(",");
-        board.play(Integer.parseInt(split2[0]),Integer.parseInt(split2[1]),2);
+        String[] split2 = cpuPlay.split(",");
+        board.play(Integer.parseInt(split2[0]), Integer.parseInt(split2[1]), 2);
     }
 
+    /**
+     * A method representing the majority of the game.
+     * While the board isnt full the game continues. If the player attempt to put a piece outside the board he is
+     * prompted to play again.
+     */
     private void play() {
-        while(!board.isFull()) {
+        while (!board.isFull()) {
             try {
                 playerPlay();
+                if(!board.isFull())
                 cpuPlay();
                 printBoard();
             } catch (ArrayIndexOutOfBoundsException ignored) {
@@ -46,6 +66,9 @@ public class Controller {
         }
     }
 
+    /**
+     * A method that prints the current state of the board.
+     */
     private void printBoard() {
         console.appendText(board.playFieldToString());
     }
